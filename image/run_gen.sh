070e2f4af842aafae83b8f4a95341f32b0736ee3
TED A. ⭕ <105297660+wattapik@users.noreply.github.com>
2023-09-30 18:17:59 +0000

    Update run_gen.sh

--- image/run_gen.sh
+++ image/run_gen.sh
@@ -4,10 +4,13 @@ source script/bin/activate
 # Xvfb :99 -screen 0 1280x1024x24 &
 # export DISPLAY=:99
 
-python3 script/generate_dzi_files.py
-python3 script/generate_images_txt.py
-python3 script/generate_filtered_photos.py
-python3 script/generate_annotations.py
+pip install pillow
+pip install requests
+
+python3.11 script/generate_dzi_files.py
+python3.11 script/generate_images_txt.py
+python3.11 script/generate_filtered_photos.py
+python3.11 script/generate_annotations.py
 
 sudo chmod -R 775 .
 sudo chown -R opc .
